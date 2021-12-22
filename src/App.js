60fa6a2660dc4f71b1f9b4b46a77daff9e9d4e36
seneiya <seneiyacooke@gmail.com>
2021-12-22 04:53:27 -0500

    button fetch done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [advice, setAdvice] = useState('')
+
+  const fetchData = () => {
+    fetch('https://api.adviceslip.com/advice')
+    .then(r => r.json())
+    .then((data) => setAdvice(data.slip.advice))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> <button onClick={() => fetchData()}> FJHSHWGJ </button> </h1>
+      <h2>{advice} </h2>
     </div>
   );
 }
